258eeeaf3b06b5539846a37d6067dcf8184df6cd
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-12-24 12:25:17 +0100

    Fix chown command and add real group.

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ rsync_ver=3.2.7
 
 sudo docker build -f Dockerfile . -t cakrome/rsync-static --build-arg RSYNC_VERSION=$rsync_ver
 sudo docker run --rm -it -v $(pwd):/workspace -w /root/build cakrome/rsync-static cp rsync-$rsync_ver.tar.xz /workspace
-sudo chown $USER:$USER rsync-$rsync_ver.tar.xz
+sudo chown $USER:`id -g` rsync-$rsync_ver.tar.xz
