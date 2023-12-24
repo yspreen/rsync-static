799735f896dabca84e2168b74ae097cd1b711533
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-12-24 12:32:43 +0100

    Add platform.

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -ex
 
 rsync_ver=3.2.7
+PLATFORM=linux/amd64
 
-sudo docker build -f Dockerfile . -t cakrome/rsync-static --build-arg RSYNC_VERSION=$rsync_ver
-sudo docker run --rm -it -v $(pwd):/workspace -w /root/build cakrome/rsync-static cp rsync-$rsync_ver.tar.xz /workspace
+sudo docker buildx build --platform $PLATFORM -f Dockerfile . -t cakrome/rsync-static --build-arg RSYNC_VERSION=$rsync_ver
+sudo docker run --platform $PLATFORM --rm -it -v $(pwd):/workspace -w /root/build cakrome/rsync-static cp rsync-$rsync_ver.tar.xz /workspace
 sudo chown $USER:`id -g` rsync-$rsync_ver.tar.xz
